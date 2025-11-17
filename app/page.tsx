05cebb6fe09945e5e55c19d3bef30bf9ebefc5d6
Dimqe <petr.makarov249@gmail.com>
2025-11-17 17:48:52 +0100

    add Join and ConfirmModal compo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
-import css from "./page.module.css";
+
+import css from './page.module.css';
+import { Join } from '@/components/Join/Join';
+
 
 export default function Home() {
-  return <main className={css.main}></main>;
+  return (
+    <main className={css.main}>
+      <Join />
+
+    </main>
+  );
 }
