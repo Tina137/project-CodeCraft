26e08c02082a6c84509c3061b5e484b8f136a745
intakie <intakiea@gmail.com>
2025-11-18 18:50:02 +0200

    add fixed edit page

--- app/stories/[storyId]/edit/page.tsx
+++ app/stories/[storyId]/edit/page.tsx
@@ -1,21 +1,10 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import AddStoryForm from "@/components/AddStoryForm/AddStoryForm";
 import { getStoryById } from "@/lib/api/clientApi";
 
-interface Props {
-  params: { storyId: string };
-}
+export default async function EditStoryPage({ params }: any) {
+  const { storyId } = await params;
 
-export default function EditStoryPage({ params }: Props) {
-  const [story, setStory] = useState<any>(null);
-
-  useEffect(() => {
-    getStoryById(params.storyId).then((data) => setStory(data));
-  }, [params.storyId]);
-
-  if (!story) return <p>Loading...</p>;
+  const story = await getStoryById(storyId);
 
   return (
     <AddStoryForm
